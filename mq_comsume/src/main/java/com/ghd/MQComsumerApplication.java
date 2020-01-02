b61b6544e53825156d1194c8c5fcf362c0e34179
gao <gao_email@163.com>
2020-01-02 11:24:13 +0800

    Merge branch 'master' of https://github.com/fullianGHD/learn-cloud
    # Conflicts:
    #	comsume/src/main/resources/application.yml
    #	comsume/target/classes/application.yml

---
+++ mq_comsume/src/main/java/com/ghd/MQComsumerApplication.java
@@ -0,0 +1,15 @@
+package com.ghd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Ghd
+ * @Date: 2020/1/2 11:20
+ */
+@SpringBootApplication
+public class MQComsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MQComsumerApplication.class,args);
+    }
+}
